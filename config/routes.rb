82df37780c8068c6b9d8f830430bd0aac32384eb
Hokuto375 <hoku0539@gmail.com>
2024-02-15 20:00:12 +0900

    babel

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tweets
   resources :sindans
-  root 'tweets#index'
+  root 'tweets#top'
 end
